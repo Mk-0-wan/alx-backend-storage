b2ef5e4082ac7b6738cbebe3f573cab92cb1f8db
gh057 <victormurithi2020@gmail.com>
2024-06-07 01:52:47 +0300

    Used Union instea of the |

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Tuple of string and a float value"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
     """returns a tuple of string and (int or float) values"""
     ret: Tuple[str, float] = (k, pow(v, 2))
     return ret
