97a331ed9fbe6e93c1c61e3e10e9157bec87520d
gh057 <victormurithi2020@gmail.com>
2024-06-07 01:50:08 +0300

    Applied typing list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,9 +6,3 @@ from typing import Union, List
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return the sum of the mixed list"""
     return sum(mxd_lst)
-
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
