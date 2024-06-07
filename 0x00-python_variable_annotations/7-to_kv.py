79382ab209532ade6f14e2657ae988c8f893fd67
gh057 <victormurithi2020@gmail.com>
2024-06-07 05:50:41 +0300

    fixed the return prototype

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple of string and (int or float) values"""
-    ret: Tuple[str, float] = (k, v**2)
+    ret: Tuple[str, float] = (k, v*v)
     return ret
