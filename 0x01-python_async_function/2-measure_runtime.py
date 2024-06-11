4d3284b887af5e0b60cef9551e5c3dcf84ba03f2
gh057 <victormurithi2020@gmail.com>
2024-06-11 06:32:09 +0300

    running async func inside a synchronous func

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,11 +5,11 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for
     wait_n(n, max_delay) and returns total_time / n."""
-    start_time = time.perf_counter()  # Start measuring time
-    await wait_n(n, max_delay)  # Await the wait_n coroutine
-    end_time = time.perf_counter()  # End measuring time
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
     total_time = end_time - start_time  # Calculate total elapsed time
     return total_time / n
