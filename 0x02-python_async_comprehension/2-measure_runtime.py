d1a3a2736f170c802c64d1fe37bfc68779cfabea
gh057 <victormurithi2020@gmail.com>
2024-06-12 01:03:16 +0300

    running parallel with asyncio.gather method

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Making parallel asychronous calls"""
 import asyncio
 import time
@@ -9,10 +8,11 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """runs parallel coroutines"""
     start: float = time.perf_counter()
-    _ = await asyncio.gather(
+   await asyncio.gather(
              async_comprehension(),
              async_comprehension(),
              async_comprehension(),
              async_comprehension(),)
     end: float = time.perf_counter()
-    return (end - start)
+    total = end - start
+    return (total)
