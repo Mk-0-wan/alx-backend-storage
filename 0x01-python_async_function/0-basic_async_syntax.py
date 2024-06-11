ef7780e68d47ecfe3d834b21925515db28ff4113
gh057 <victormurithi2020@gmail.com>
2024-06-11 03:53:49 +0300

    passing correct parameters

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = random.uniform(0, 10)) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """return a radmom float value"""
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(max_delay)
     return max_delay
