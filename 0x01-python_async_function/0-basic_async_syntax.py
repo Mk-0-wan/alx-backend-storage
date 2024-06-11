5643537bf288b8074e0384eeda62f6d3564673d4
gh057 <victormurithi2020@gmail.com>
2024-06-11 03:19:01 +0300

    Making asnychronous function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Making a co-oroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = random.uniform(0, 10))-> float:
+    """return a radmom float value"""
+    await asyncio.sleep(max_delay)
+    return max_delay
