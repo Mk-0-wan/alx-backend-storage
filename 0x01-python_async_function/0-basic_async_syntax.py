421c8a88fb93b62c00144ed09d4948ca2c6d674b
gh057 <victormurithi2020@gmail.com>
2024-06-11 03:40:50 +0300

    fixing linting issues

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Making a co-oroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay = random.uniform(0, 10)):
+    """return a radmom float value"""
+    await asyncio.sleep(max_delay)
+    return max_delay
