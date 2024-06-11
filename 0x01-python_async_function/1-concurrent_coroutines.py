54c09c0c33909bb04b6c44a20a75b1e56af0e91b
gh057 <victormurithi2020@gmail.com>
2024-06-11 05:07:59 +0300

    fixing the inserting method

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,9 +4,26 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def fix_array(arry: list, elem: float) -> list:
+    """Returns a sorted array in an ascending order"""
+    if not arry:
+        arry.append(elem)
+        return arry
+    for idx in range(len(arry)):
+        if elem < arry[idx]:
+            arry.insert(idx, elem)
+            return
+    arry.append(elem)
+
+
 async def wait_n(n: int, max_delay: int) -> list:
     """Making mulitiple concurrency co-routine"""
+    # make a list of all the co-routines returned
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     await_list = []
-    for _ in range(n):
-        await_list.append(await wait_random(max_delay))
+    # append to th list all the early task completed in the tasks routine
+    for task in asyncio.as_completed(tasks):
+        finished = await task
+        fix_array(await_list, finished)
+
     return await_list
