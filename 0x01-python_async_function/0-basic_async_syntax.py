6062481c3ac0c4cb6f5fe3d955242b9baa57939e
gh057 <victormurithi2020@gmail.com>
2024-06-11 05:20:31 +0300

    fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """return a radmom float value"""
     delay = random.uniform(0, max_delay)
-    await asyncio.sleep(max_delay)
-    return max_delay
+    await asyncio.sleep(delay)
+    return delay
