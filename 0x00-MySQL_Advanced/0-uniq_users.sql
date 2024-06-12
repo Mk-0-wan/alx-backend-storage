cb05154b2bc1b3e1fc7947e76ef6509ba5fa5d23
mk <victormurithi2020@gmail.com>
2024-06-13 00:20:42 +0300

    creating a table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Sql code that creates a new table called users
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY(id)
+);
