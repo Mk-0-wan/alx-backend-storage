3f4386d65acf68568e556855a77e224435d32e70
mk <victormurithi2020@gmail.com>
2024-06-14 00:21:29 +0300

    arranging in ascending order

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Creating Multi-Indexing mode for even faster data lookup
-CREATE INDEX idx_name_first_score ON names ((name(1), score ASC));
+CREATE INDEX idx_name_first_score ON names (name(1), score ASC);
