7cf1a247f437727199cbd3a5b56d2aa0dd5a4ce2
gh057 <victormurithi2020@gmail.com>
2024-06-18 09:17:20 +0300

    updated the topics attributes

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,7 +2,7 @@
 """Creating a new attribute"""
 
 
-def schools_by_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, name, topic):
     """Updating the topics attribute in a document collection"""
     updated = mongo_collection.update_one({'name': name}, {'$set': {'topics': topic}})
     return updated
