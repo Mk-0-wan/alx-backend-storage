73b7acf5e556e7cffee3593572161452da013742
gh057 <victormurithi2020@gmail.com>
2024-06-20 02:07:55 +0300

    all data should be in bytes form or string form

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import uuid
 import redis
-from typing import Any, Union
+from typing import Any, Union, Callable
 
 
 class Cache:
@@ -24,3 +24,39 @@ class Cache:
 
         self._redis.set(gen_key, data_to_store)
         return gen_key
+
+    def get_str(self, key: Any) -> str:
+        """returns the correct value(str type) from the get method"""
+        return str(self._redis.get(key))
+
+    def get_int(self, key: Any) -> str:
+        """returns the correct value(str type) from the get method"""
+        return int(self._redis.get(key))
+
+    def get(key: Union[str, int, bytes], fn: Callable[[Any], Union[str, int]) -> Any:
+        """ A get method that take a key string argument and an optional
+            Callable argument named fn.
+            This callable will be used to convert the data back to the desired format.
+        """
+        try:
+            if type(key) == str:
+                ret = get_str(key)
+            elif type(key) == int:
+                ret = get_str(key)
+            return ret
+        except as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
