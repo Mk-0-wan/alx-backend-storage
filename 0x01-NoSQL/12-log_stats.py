0d8424b3336a8a8e271bdff42f8e2c3bc97f8d9c
gh057 <victormurithi2020@gmail.com>
2024-06-19 15:32:35 +0300

    counting one by one

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -22,18 +22,32 @@ def search_method_and_path(mongo_coll):
     """returns the total count of docs matching the
     search_method and path attribute"""
     return mongo_coll.count_documents({
-        "method": "GET",
         "path": "/status"
         })
 
 
 if __name__ == '__main__':
-    client = MongoClient('mongodb://localhost:27017')
-    nginx_collection = client.logs.nginx
-    search_method = search_method_used(nginx_collection)
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_coll = client.logs.nginx
+    search_method = search_method_used(nginx_coll)
 
-    print("{} logs".format(count(nginx_collection)))
+    print("{} logs".format(count(nginx_coll)))
     print("Methods:")
-    for method in search_method:
-        print("\tmethod {}: {}".format(method, search_method[method]))
-    print("{} status check".format(search_method_and_path(nginx_collection)))
+    # for method in search_method:
+    # print("\tmethod {}: {}".format(method, search_method[method]))
+    print("\tmethod GET: {}".format(
+        nginx_coll.count_documents({"method": "GET"})
+        ))
+    print("\tmethod POST: {}".format(
+        nginx_coll.count_documents({"method": "POST"})
+        ))
+    print("\tmethod PUT: {}".format(
+        nginx_coll.count_documents({"method": "PUT"})
+        ))
+    print("\tmethod PATCH: {}".format(
+        nginx_coll.count_documents({"method": "PATCH"})
+        ))
+    print("\tmethod DELETE: {}".format(
+        nginx_coll.count_documents({"method": "DELETE"})
+        ))
+    print("{} status check".format(search_method_and_path(nginx_coll)))
